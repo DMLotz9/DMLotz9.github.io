9cf5390bb51bc39dd2984f5fb848ccffa1ffaab1
Dylan Lotz <dschnake09@yahoo.com>
2021-06-04 15:46:59 -0500

    Testing db

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ window.onload=function(){
     fetch("https://dmlotz9.github.io/Data/moods.json").then(function(response){
        response.json().then(function(json){
        document.getElementById("moodStatus").innerHTML= `      <h2>Current Status</h2>
-       <ul>
-          <li>Current Mood: ${json[0].Mood}</li>
-         </ul>
-       <img src="${json[0].Image}"></img>
-       <ul>
-       <li>Action Plan: ${json[0].Description}</li>
-       </ul>`;
+          <p>Current Mood: ${json[0].Mood}</p>
+          <img src="${json[0].Image}"></img>
+          <p>Action Plan: ${json[0].Description}</p>`;
        }); 
     });
  };
