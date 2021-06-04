f3262662b1c37a8626e0c09e91c3b25848e6bbce
Dylan Lotz <dschnake09@yahoo.com>
2021-06-04 13:58:28 -0500

    Updates json file

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.onload=function(){
-    fetch("https://github.com/DMLotz9/MoodTrackerv2/blob/29ae2146e6a081231cf1b9c6c15731441e7242e1/mood.json").then(function(response){
+    fetch("https://dmlotz9.github.io/Data/moods.json").then(function(response){
        response.json().then(function(json){
        document.getElementById("moodStatus").innerHTML= `      <h2>Current Status</h2>
-       <ol>
+       <ul>
           <li>Current Mood: ${json[0].Mood}</li>
-          <li>Action Plan: ${json[0].Description}</li>
-       </ol>
-       <img src="${json[0].Image}"></img>`;
+         </ul>
+       <img src="${json[0].Image}"></img>
+       <ul>
+       <li>Action Plan: ${json[0].Description}</li>
+       </ul>`;
        }); 
     });
  };
